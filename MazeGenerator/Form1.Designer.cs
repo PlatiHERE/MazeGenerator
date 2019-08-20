04da777cd03fe8182d7e8335607e68fd1543516c
PlatiHERE <sieronmarcel@gmail.com>
2019-08-20 19:11:00 +0200

    Dodaj pliki projektów.

---
+++ MazeGenerator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MazeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Maze size:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 12);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(167, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Generate Live";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 553);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MazeGenerator/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            Generator generator = new Generator((int)numericUpDown1.Value);
+            pictureBox1.Image = generator.Draw();
+        }
+    }
+
+    public class Generator {
+
+        int seekerPosX;
+        int seekerPosY;
+        int size;
+        Cell[,] cell;
+
+        public Generator(int size) {
+            this.size = size;
+            seekerPosX = size / 2;
+            seekerPosY = 0;
+            cell = new Cell[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    cell[j, i] = new Cell();
+                }
+            }
+            
+        }
+
+        void Direction() {
+            Random random = new Random();
+            List<Point> list = CheckNeighbors();
+            if (list.Count > 0)
+            {
+                int val = random.Next(list.Count - 1);
+                seekerPosX = list[val].X;
+                seekerPosY = list[val].Y;
+            }
+        }
+
+        List<Point> CheckNeighbors() {
+            List<Point> list = new List<Point>();
+            if (seekerPosY > 0    &&    !cell[seekerPosX,     seekerPosY - 1].visited) list.Add(new Point(seekerPosX,     seekerPosY - 1));    //UP
+            if (seekerPosX < size &&    !cell[seekerPosX + 1, seekerPosY    ].visited) list.Add(new Point(seekerPosX + 1, seekerPosY));        //RIGHT
+            if (seekerPosY < size &&    !cell[seekerPosX,     seekerPosY + 1].visited) list.Add(new Point(seekerPosX,     seekerPosY + 1));    //DOWN
+            if (seekerPosX > 0    &&    !cell[seekerPosX - 1, seekerPosY    ].visited) list.Add(new Point(seekerPosX - 1, seekerPosY));        //LEFT
+            return list;
+        }
+
+        public Bitmap Draw() {
+            Bitmap bmp = new Bitmap((size + 2) * 10 + (size + 2) * 2, (size + 2) * 10 + (size + 2) * 2);     //Create Bitmap. 10px per cell. 2px per space between cells.
+            Graphics graphics = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black, 1);
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        cell[j, i].Draw(pen, graphics, new Point(j, i), 10);
+            //    }
+            //}
+            cell[0, 0].Draw(pen, graphics, new Point(0, 0), 10);
+            return bmp;
+        }
+    }
+    public class Cell {
+        public bool[] walls = { true, true, true, true };   // top, right, bottom, left
+        public bool visited = false;
+
+        public Cell() {
+
+        }
+
+        public void Draw(Pen pen, Graphics graphics, Point position, int cellPxSize) {
+
+            if (walls[0]) graphics.DrawLine(pen, (position.X + 1 * cellPxSize) - pen.Width,                  //     LEFT X
+                                                 (position.Y + 1 * cellPxSize) - pen.Width,                  //     TOP Y
+                                                 (position.X + 1 * cellPxSize) + cellPxSize + pen.Width,     //     RIGHT X
+                                                 (position.Y + 1 * cellPxSize) - pen.Width);                 //     TOP Y
+
+            if (walls[1]) graphics.DrawLine(pen, (position.X + 1 * cellPxSize) + cellPxSize + pen.Width,     //     RIGHT X
+                                                 (position.Y + 1 * cellPxSize) - pen.Width,                  //     TOP Y
+                                                 (position.X + 1 * cellPxSize) + cellPxSize + pen.Width,     //     RIGHT X
+                                                 (position.Y + 1 * cellPxSize) + cellPxSize + pen.Width);    //     BOTTOM Y
+
+            if (walls[1]) graphics.DrawLine(pen, (position.X + 1 * cellPxSize) + cellPxSize + pen.Width,     //     RIGHT X
+                                                 (position.Y + 1 * cellPxSize) + cellPxSize + pen.Width,     //     BOTTOM Y
+                                                 (position.X + 1 * cellPxSize) - pen.Width,                  //     LEFT X
+                                                 (position.Y + 1 * cellPxSize) + cellPxSize + pen.Width);    //     BOTTOM Y
+
+            if (walls[1]) graphics.DrawLine(pen, (position.X + 1 * cellPxSize) - pen.Width,                  //     LEFT X
+                                                 (position.Y + 1 * cellPxSize) - pen.Width,                  //     TOP Y
+                                                 (position.X + 1 * cellPxSize) - pen.Width,                  //     LEFT X
+                                                 (position.Y + 1 * cellPxSize) + cellPxSize + pen.Width);    //     BOTTOM Y
+        }
+
+    }
+
+}
